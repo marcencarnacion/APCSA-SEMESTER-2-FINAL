dbe01831e0b17e42c5777aa640f913dea9b5f6ae
Marc Christian Encarnacion <>
2021-06-03 07:28:00 +0000

    Added labels to the tabs and put a framework for the payment method

---
+++ ItemsParent.java
@@ -0,0 +1,38 @@
+public abstract class ItemsParent 
+{
+  //1 Instance Variables 
+  public String itemName;
+  public int price;
+  public boolean isOnList;
+
+  //2 Constructor
+  public ItemsParent(String itemName, int price, boolean isOnList)
+  {
+    this.itemName = itemName;
+    this.price = price;
+    this.isOnList = isOnList;
+  }//end ItemsParent constructor (3 Argument)
+
+  public ItemsParent()
+  {
+    this.itemName = ";
+    this.price = 0;
+    this.isOnList = false;
+  }//end ItemsParent constructor (0 Argument)
+  //Getters and Setters
+  public String itemName()
+  {
+    return itemName;
+  }//end itemName getter
+
+  public void setName(String itemName)
+  {
+    this.itemName = itemName;
+  }//brain method
+  
+  //3 toString
+  public String toString()
+  {
+    return "Name: " + itemName + "\n Price: " + price + "\n On List?: " + isOnList;
+  }//end toString
+}//end class Restaurant
--- JTabbedPaneFrame.java
+++ JTabbedPaneFrame.java
@@ -10,7 +10,7 @@ public class JTabbedPaneFrame extends JFrame
 {
   public JTabbedPaneFrame()
   {
-    super("JTabbedPaneFrame Demo");
+    super("Rioux Store Simulator");
 
     //create the tabbed pane/ holds tab formatting
     JTabbedPane tabPane = new JTabbedPane();
@@ -19,31 +19,38 @@ public class JTabbedPaneFrame extends JFrame
     JLabel label1 = new JLabel ("Welcome to Rioux Mart", SwingConstants.CENTER);
     JPanel panel1 = new JPanel();
     panel1.add(label1);
-    tabPane.addTab("Tab One", null, panel1, "FirstPanel");
+    tabPane.addTab("Welcome", null, panel1, "FirstPanel");
 
     //add second tab
     JLabel label2 = new JLabel ("Objective: Please select items to fill out your grocery list", SwingConstants.CENTER);
     JPanel panel2 = new JPanel();
     panel2.add(label2);
-    tabPane.addTab("Tab Two", null, panel2, "SecondPanel");
+    //In case you want to add buttons on the sides=
+    //panel2.setLayout(new BorderLayout());
+    panel2.add(new JButton("Carrots"), BorderLayout.NORTH);
+    panel2.add(new JButton("Lettuce"), BorderLayout.SOUTH);
+    panel2.add(new JButton("Cereal"), BorderLayout.WEST);
+    panel2.add(new JButton("Steak"), BorderLayout.EAST);
+    panel2.add(new JButton("Potato"), BorderLayout.EAST);
+    panel2.add(new JButton("Chips"), BorderLayout.SOUTH);
+    panel2.add(new JButton("Juice"), BorderLayout.SOUTH);
+    panel2.add(new JButton("Bread"), BorderLayout.SOUTH);
+    panel2.add(new JButton("Pasta"), BorderLayout.SOUTH);
+    panel2.add(new JButton("Eggs"), BorderLayout.SOUTH);
+    tabPane.addTab("Select Items", null, panel2, "SecondPanel");
 
     //add third tab
-    JLabel label3 = new JLabel ("These are fabulous buttons here", SwingConstants.CENTER);
+    JLabel label3 = new JLabel ("Please pay here. (+/-)", SwingConstants.CENTER);
     JPanel panel3 = new JPanel();
     panel3.setLayout(new BorderLayout());
     panel3.add(label3);
-    panel3.add(new JButton("North"), BorderLayout.NORTH);
-    panel3.add(new JButton("South"), BorderLayout.SOUTH);
-    panel3.add(new JButton("West"), BorderLayout.WEST);
-    panel3.add(new JButton("East"), BorderLayout.EAST);
-    tabPane.addTab("Tab Three", null, panel3, "ThirdPanel");//keep the add below al the tab's formatting
-
-  
-
+    //panel3.add(new JButton("North"), BorderLayout.NORTH);
+    panel3.add(new JButton("Pay"), BorderLayout.SOUTH);
+    panel3.add(new JButton("Increase"), BorderLayout.WEST);
+    panel3.add(new JButton("Decrease"), BorderLayout.EAST);
+    tabPane.addTab("Payment", null, panel3, "ThirdPanel");//keep the add below al the tab's formatting
 
     //keep this at the bottom
     add(tabPane);
-
   }//end default and only constructor
-
 }//end class
